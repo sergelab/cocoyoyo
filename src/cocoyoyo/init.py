c75cb4402217c3e5948e5a480e415cd96181bad2
Sergelab <serge@brpr.ru>
2014-07-10 22:35:53 +0400

    Added blueprint registration

--- src/cocoyoyo/init.py
+++ src/cocoyoyo/init.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import importlib
+
+from flask import Flask, Blueprint
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
@@ -15,7 +17,28 @@ def create_app():
     return app
 
 
+def register_blueprints(app):
+    rv = []
+
+    if not app.config['BLUEPRINTS']:
+        app.config['BLUEPRINTS'] = []
+
+    for name in app.config['BLUEPRINTS']:
+        m = importlib.import_module(name + '.views')
+        for item in dir(m):
+            item = getattr(m, item)
+            if isinstance(item, Blueprint):
+                app.register_blueprint(item)
+                app.logger.debug('Blueprint "{0}" registered'.format(name))
+                rv.append(item)
+
+    return rv
+
+
 app = create_app()
+
+with app.app_context():
+    register_blueprints(app)
 
 import models
 import views
